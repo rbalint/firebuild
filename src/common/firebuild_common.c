fd3097187608e2ca1f710c5c0d72d3134e934607
Balint Reczey <balint@balintreczey.hu>
2024-10-24 18:03:12 +0200

    interceptor: Use custom heapsort() with alloca() instead of qsort() to avoid malloc()-s

--- src/common/firebuild_common.c
+++ src/common/firebuild_common.c
@@ -51,7 +51,7 @@ static int cmp_cstring_view(const void *p1, const void *p2) {
 
 void cstring_view_array_sort(cstring_view_array *array) {
   if (array->p) {
-    qsort(array->p, array->len, sizeof(cstring_view), cmp_cstring_view);
+    heapsort_alloca(array->p, array->len, sizeof(cstring_view), cmp_cstring_view);
   }
 }
 
